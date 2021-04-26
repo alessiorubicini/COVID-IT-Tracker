ab5fbd5b48de87e4552d3a7bad7106b561ff5919
Alessio Rubicini <60115008+alessiorubicini@users.noreply.github.com>
2021-04-26 14:51:55 +0200

    Cleaning repository

--- database/data-insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Zone (id, colore) VALUES (1, 'Giallo');
-INSERT INTO Zone (id, colore) VALUES (2, 'Arancione');
-INSERT INTO Zone (id, colore) VALUES (3, 'Rosso');
-INSERT INTO Zone (id, colore) VALUES (4, 'Bianco');
-
-
